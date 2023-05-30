ecbb198a93c283f6a5b4673461cc52c9f6e07c3b
Mike <michaelambrosino93@gmail.com>
2023-05-30 14:58:18 -0400

    implement full basic arithmetic functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,27 @@
+let firstOperand, currentOperator;
+let shouldClearScreen = false;
+
+let display = document.querySelector("div.display > output");
+let equals = document.getElementById("equals");
+let numberButtons = document.querySelectorAll("button.number");
+let operators = document.querySelectorAll("button.operator");
+let clear = document.getElementById("clear");
+
+clear.addEventListener("click", () => clearAll());
+equals.addEventListener("click", () => evaluate());
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => appendDisplay(button.id));
+});
+
+operators.forEach((operatorButton) => {
+  operatorButton.addEventListener("click", () => {
+    setOperation(operatorButton.id);
+  });
+});
+
+clearAll();
+
 const operations = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
@@ -5,92 +29,49 @@ const operations = {
   divide: (a, b) => a / b,
 };
 
-let stored, operator;
-let equalsStored;
-
-let display = document.querySelector("div.display > output");
-display.textContent = "0";
-let lastPressed = "number";
-
-const operate = (a, b, operator) => {
+function operate(a, b, operator) {
   return operations[operator](a, b);
-};
+}
 
-const appendDisplay = (digit) => {
+function clearScreen() {
+  display.textContent = "0";
+  shouldClearScreen = false;
+}
+
+function getDisplayValue() {
+  return Number.parseInt(display.textContent);
+}
+
+function clearAll() {
+  clearScreen();
+  firstOperand = null;
+  operator = null;
+  shouldClearScreen = false;
+}
+
+function appendDisplay(digit) {
+  console.log("here");
+  if (shouldClearScreen) clearScreen();
   let newDisplay =
     display.textContent == "0" ? digit : display.textContent + digit;
 
   display.textContent = newDisplay;
-};
+}
 
-const clearDisplay = () => {
-  display.textContent = "0";
-};
+function setOperation(operator) {
+  if (currentOperator != null) evaluate();
+  firstOperand = getDisplayValue();
+  currentOperator = operator;
+  shouldClearScreen = true;
+}
 
-const clearAll = () => {
-  clearDisplay();
-  stored = null;
-  operator = null;
-};
-
-const getDisplayVal = () => {
-  return Number.parseInt(display.textContent);
-};
-
-let numberButtons = document.querySelectorAll("button.number");
-
-numberButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    appendDisplay(Number.parseInt(button.id));
-    lastPressed = "number";
-  });
-});
-
-let clear = document.getElementById("clear");
-
-clear.addEventListener("click", clearAll);
-
-let operators = document.querySelectorAll("button.operator");
-
-operators.forEach((operatorButton) => {
-  operatorButton.addEventListener("click", (e) => {
-    if (lastPressed == "operator") {
-      operator = operatorButton.id;
-    } else {
-      stored = getDisplayVal();
-      display.textContent = "0";
-      operator = operatorButton.id;
-    }
-
-    console.table({
-      stored: stored,
-      display: getDisplayVal(),
-      op: operator,
-    });
-
-    lastPressed = "operator";
-  });
-});
-
-let equals = document.getElementById("equals");
-
-equals.addEventListener("click", (e) => {
-  let displayVal = getDisplayVal();
-
-  if (lastPressed == "equals") {
-    display.textContent = operate(displayVal, equalsStored, operator);
-  } else {
-    let result = operate(stored, displayVal, operator);
-    equalsStored = displayVal;
-    display.textContent = result;
-  }
-
-  lastPressed = "equals";
-
-  console.table({
-    stored: stored,
-    display: getDisplayVal(),
-    op: operator,
-    equals: equalsStored,
-  });
-});
+function evaluate() {
+  if (currentOperator == null || shouldClearScreen) return;
+  display.textContent = operate(
+    firstOperand,
+    getDisplayValue(),
+    currentOperator
+  );
+  currentOperator = null;
+  shouldClearScreen = true;
+}
